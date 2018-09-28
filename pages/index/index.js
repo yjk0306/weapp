da9463cea96d758c18eff3251bd2bea22d147cd8
jikai.yang <jikai.yang@jitstack.com>
2018-09-28 18:05:43 +0800

    初始化

---
+++ pages/index/index.js
@@ -0,0 +1,78 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    imgUrls: [
+      '../image/001.jpg',
+      '../image/002.jpg',
+      '../image/003.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000,
+    classify:[
+      { src:"../image/fruit1.png",  names:"拼团"  },
+      { src: "../image/fruit2.png", names: "新品" },
+      { src: "../image/fruit3.png", names: "限时特惠" },
+      { src: "../image/fruit4.png", names: "砍价" },
+      { src: "../image/fruit5.png", names: "特卖" }, 
+      { src: "../image/fruit6.png", names: "预约" },
+      { src: "../image/fruit7.png", names: "优惠券" },
+      { src: "../image/fruit8.png", names: "爆款" }
+
+    ]
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    
+    // wx.navigateTo({
+    //   url: '../logs/logs'
+    // })
+  },
+  finds (e) {
+      console.log(e)
+  },
+  onLoad: function () {
+    // if (app.globalData.userInfo) {
+    //   this.setData({
+    //     userInfo: app.globalData.userInfo,
+    //     hasUserInfo: true
+    //   })
+    // } else if (this.data.canIUse){
+    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //   // 所以此处加入 callback 以防止这种情况
+    //   app.userInfoReadyCallback = res => {
+    //     this.setData({
+    //       userInfo: res.userInfo,
+    //       hasUserInfo: true
+    //     })
+    //   }
+    // } else {
+    //   // 在没有 open-type=getUserInfo 版本的兼容处理
+    //   wx.getUserInfo({
+    //     success: res => {
+    //       app.globalData.userInfo = res.userInfo
+    //       this.setData({
+    //         userInfo: res.userInfo,
+    //         hasUserInfo: true
+    //       })
+    //     }
+    //   })
+    // }
+  },
+  getUserInfo: function(e) {
+    // console.log(e)
+    // app.globalData.userInfo = e.detail.userInfo
+    // this.setData({
+    //   userInfo: e.detail.userInfo,
+    //   hasUserInfo: true
+    // })
+  }
+})
